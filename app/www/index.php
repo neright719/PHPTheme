975adbf3474f0d14b09f061f0074fda593ff6273
neright719 <95737594+neright719@users.noreply.github.com>
2022-01-31 15:55:39 +0900

    updated this file

--- app/www/index.php
+++ app/www/index.php
@@ -58,6 +58,8 @@ $csrf_token = Token::Generate();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/common.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.js"></script>
+    <script src="/js/main.js"></script>
     <title>ダッシュボード / CRUD-App</title>
 </head>
 <body>
