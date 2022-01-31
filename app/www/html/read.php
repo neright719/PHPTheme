2040fac8efd1f7313a2589bb06821ebe582cf765
neright719 <95737594+neright719@users.noreply.github.com>
2022-01-31 15:56:27 +0900

    updated this file

--- app/www/html/read.php
+++ app/www/html/read.php
@@ -16,8 +16,21 @@
             <span class="column_mail"><?= $record["email"]; ?></span>
             <span class="column_created_at"><?= $record["created_at"]; ?></span>
             <span class="column_updated_at"><?= $record["updated_at"]; ?></span>
-            <span class="column_update"><a class="update_btn" href="/?action=update&id=<?= $record["id"]; ?>"><i class="fas fa-edit"></i></a></span>
-            <span class="column_delete"><a class="delete_btn" href="/?action=delete&id=<?= $record["id"]; ?>"><i class="fas fa-trash-alt"></i></a></span>
+            <span class="column_update">
+                <a class="update_btn" 
+                    data-id="<?= $record["id"]; ?>"
+                    data-mail="<?= $record["email"]; ?>"
+                    data-created_at="<?= $record["created_at"]; ?>"
+                    data-updated_at="<?= $record["updated_at"]; ?>"
+                    href="/?action=update&id=<?= $record["id"]; ?>">
+                    <i class="fas fa-edit"></i>
+                </a>
+            </span>
+            <span class="column_delete">
+                <a class="delete_btn" data-csrf_token="<?= $csrf_token; ?>" data-id="<?= $record["id"]; ?>" href="/?action=delete&id=<?= $record["id"]; ?>">
+                    <i class="fas fa-trash-alt"></i>
+                </a>
+            </span>
         </div>
         <?php endforeach; ?>
     </div>
